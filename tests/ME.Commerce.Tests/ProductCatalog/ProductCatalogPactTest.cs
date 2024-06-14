d1ec52752f60e199e444c860ed071816c147eccc
Kellyson Santos <kellyson.victor@gmail.com>
2024-06-14 19:29:45 -0300

    Use PactBroker

--- tests/ME.Commerce.Tests/ProductCatalog/ProductCatalogPactTest.cs
+++ tests/ME.Commerce.Tests/ProductCatalog/ProductCatalogPactTest.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using FluentAssertions;
+using FluentAssertions.Extensions;
 using ME.Commerce.Core.Features.ProductListing.Events;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
@@ -19,6 +20,8 @@ namespace ME.Commerce.Tests.ProductCatalog
     public class ProductCatalogPactTest : IDisposable
     {
         private static readonly Uri _providerUri = new("http://localhost:5000");
+
+        private static readonly Uri _pactServerUri = new("http://localhost:9292");
 
         private static readonly JsonSerializerOptions _options =
             new()
@@ -46,7 +49,7 @@ namespace ME.Commerce.Tests.ProductCatalog
             _server.Start();
 
             _verifier = new PactVerifier(
-                "Product Catalog",
+                "ProductCatalogApi",
                 new PactVerifierConfig
                 {
                     LogLevel = PactLogLevel.Debug,
@@ -95,6 +98,32 @@ namespace ME.Commerce.Tests.ProductCatalog
                 .WithProviderStateUrl(new Uri(_providerUri, "/provider-states"))
                 .Verify();
         }
+
+        [Fact]
+        public void EnsureProductApiHonoursPactWithConsumerAndPublishResultsOnPactBroker()
+        {
+             _verifier
+                    .WithHttpEndpoint(_providerUri)
+                    .WithPactBrokerSource(_pactServerUri, options =>
+                     {
+                         options.ConsumerTags("main", "latest")
+                                .ConsumerVersionSelectors(new ConsumerVersionSelector { DeployedOrReleased = true },
+                                                          new ConsumerVersionSelector { MainBranch = true, Latest = true })
+                                .EnablePending()
+                                .PublishResults($"{DateTime.UtcNow}", publish =>
+                                {
+                                    publish.BuildUri(new Uri("https://ci.example.org/builds/1234"))
+                                           .ProviderBranch("main")
+                                           .ProviderTags("main", "latest");
+                                });
+                     })
+                    .WithProviderStateUrl(new Uri(_providerUri, "/provider-states"))
+                    //.WithFilter("description", "state")
+                    .WithRequestTimeout(10.Seconds())
+                    .WithSslVerificationDisabled()
+                    .Verify();
+        }
+
 
         private bool IsServerUp()
         {
